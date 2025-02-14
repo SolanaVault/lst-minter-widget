e34482bc725519f436a09a8faa70705e5d0ff89d
FuzzyYeti <fuzzyyeti@users.noreply.github.com>
2025-02-14 00:56:04 -0700

    add ablity to mint vsol. Remove need to deconstruct message.

--- packages/example/src/LstMinterComponent.tsx
+++ packages/example/src/LstMinterComponent.tsx
@@ -5,11 +5,13 @@ import {
 } from "@solana/web3.js";
 import bs58 from "bs58";
 import { useState } from "react";
+import { VSOL_MINT } from '@the-vault/lst-minter-api/dist/consts';
 
 export const LstMinterComponent = () => {
   const api = import.meta.env.VITE_API_URL;
   const [processing, setProccessing] = useState(false);
   const [preparing, setPreparing] = useState(false);
+  const [isGridSol, setIsGridSol] = useState(true);
   const { publicKey, wallet } = useWallet();
   const { connection } = useConnection();
   const sendTransaction = async (
@@ -44,32 +46,35 @@ export const LstMinterComponent = () => {
     }
   };
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyItems: "flex-start",
-        gap: ".5rem",
-      }}
-    >
-      <div style={{ color: "white", fontSize: "36px" }}>Stake SOL with </div>
-      <div style={{ color: "#FF8C00", fontSize: "36px" }}>Grid Systems Validator</div>
-      <LstMinterWidget
-        onButtonPress={() => {
-          setPreparing(true);
-          setTimeout(() => {
-            setPreparing(false);
-          }, 10000);
-        }}
-        onTxReady={({ transaction }) =>
-          sendTransaction(transaction)
-        }
-        api={api}
-        processing={preparing || processing}
-        mint={"gridBR1TSJcV1JjAEE9g7ouoiVaEgDNT3dhY6n9oKQq"}
-        address={publicKey?.toBase58()}
-      />
-    </div>
+      <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyItems: "flex-start",
+            gap: ".5rem",
+          }}
+      >
+        <button className="switch-button" onClick={() => setIsGridSol((prev) => !prev)}>
+          Switch to {isGridSol ? "The Vault" : "Grid Systems Validator"}
+        </button>
+        <div style={{color: "white", fontSize: "36px"}}>Stake SOL with</div>
+        <div style={{color: "#FF8C00", fontSize: "36px"}}>{isGridSol ? "Grid Systems Validator" : "The Vault"}</div>
+        <LstMinterWidget
+            onButtonPress={() => {
+              setPreparing(true);
+              setTimeout(() => {
+                setPreparing(false);
+              }, 10000);
+            }}
+            onTxReady={({transaction}) =>
+                sendTransaction(transaction)
+            }
+            api={api}
+            processing={preparing || processing}
+            mint={isGridSol ? "gridBR1TSJcV1JjAEE9g7ouoiVaEgDNT3dhY6n9oKQq" : VSOL_MINT}
+            address={publicKey?.toBase58()}
+        />
+      </div>
   );
 };
--- packages/lst-minter-widget/src/hooks/useDstInfo.ts
+++ packages/lst-minter-widget/src/hooks/useDstInfo.ts
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
+import { VSOL_MINT } from '../constants';
 
 export function useDstInfo(mint: string, api: string) {
   return useQuery({
     queryKey: ["dstInfo", mint],
     queryFn: async () => {
+      if(mint === VSOL_MINT) {
+        return null;
+      }
       const response = await fetch(`${api}/dstInfo?mint=${mint}`);
 
       if (!response.ok) {
